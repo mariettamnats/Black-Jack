7e2adb8e13271588c20fead2cc35dec366f124df
Marietta Mnatsakanian <marietta_mnats@hotmail.com>
2015-11-25 01:28:29 -0500

    work in progress

---
+++ newscript.js
@@ -0,0 +1,18 @@
+function myCards() {
+					document.getElementById("outcome").innerHTML=" ";
+						document.getElementById("dnscore").innerHTML=" ";
+						document.getElementById("dhit").innerHTML=" ";
+						document.getElementById("nscore").innerHTML=" ";
+						document.getElementById("card3").innerHTML=" ";
+					
+var cards = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace ]
+
+var ace = {
+	value: 11;
+}
+
+
+
+var 
+
+if (cards[x])
